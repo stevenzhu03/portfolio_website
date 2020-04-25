97905aafeab62821ee7b75ead7a0af761929c331
Steven Zhu <stevenzhu03@gmail.com>
2020-04-24 23:17:07 -0400

    routing pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route } from "react-router-dom"
+import AboutMe from './components/AboutMe';
+import Projects from "./components/Projects"
+import Resume from "./components/Resume"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div>
+      
+      <Route exact path="/about_me" component={AboutMe} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/resume" component={Resume} />
     </div>
   );
 }
