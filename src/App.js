713605256a54043df79e74833db1bb03fa1a8d34
Steven Zhu <stevenzhu03@gmail.com>
2020-04-26 14:04:14 -0400

    creating landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import { Route } from "react-router-dom"
-import AboutMe from './components/AboutMe';
-import Projects from "./components/Projects"
-import Resume from "./components/Resume"
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div>
-      
+      <LandingPage />
       <Route exact path="/about_me" component={AboutMe} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/resume" component={Resume} />
---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = props => {
+    return (
+        <div className="landing-page">
+            <h1>Hello, I'm <span className="landing-page-name">Steven Zhu</span></h1>
+            <h2>A software engineer based in <span className="landing-page-city">New York City</span></h2>
+        </div>
+    )
+}
+
+export default LandingPage
