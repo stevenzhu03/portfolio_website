f472586f2e20e149ffcd0e610cf513437db70dd6
Steven Zhu <stevenzhu03@gmail.com>
2020-04-26 15:14:17 -0400

    styling landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const LandingPage = props => {
-    return (
-        <div className="landing-page">
-            <h1>Hello, I'm <span className="landing-page-name">Steven Zhu</span></h1>
-            <h2>A software engineer based in <span className="landing-page-city">New York City</span></h2>
-        </div>
-    )
-}
+const LandingPage = (props) => {
+  return (
+    <div className="landing-page">
 
-export default LandingPage
+      <div className="landing-page-headings">
+        <h1>
+          Hello, I'm <span className="landing-page-name">Steven Zhu</span>.
+        </h1>
+        <h2>
+          A software engineer based in{" "}
+          <span className="landing-page-city">New York City</span>.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
