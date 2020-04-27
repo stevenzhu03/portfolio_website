6a174f6b0fe0f9409053cb6b6fb1fccab374d0dc
Steven Zhu <stevenzhu03@gmail.com>
2020-04-26 22:09:31 -0400

    working on about me page

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = props => {
+
+    return (
+        <div className="about-me-page">
+            NavBar
+        </div>
+    )
+    
+}
+
+export default NavBar
