bbb98b570e22112591d09b14ef537ca96d1adc3a
Steven Zhu <stevenzhu03@gmail.com>
2020-05-02 19:37:43 -0400

    working on about me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 
-const AboutMe = props => {
+const AboutMe = (props) => {
+  return (
+    <div className="about-me-page" id="about_me">
+      <div className="about-me-photo">
+        <Avatar
+          style={{
+            margin: "10px",
+            width: "550px",
+            height: "550px",
+          }}
+          src={require("../assets/headshot.png")}
+          alt="steven-zhu-profile"
+        />
+      </div>
 
-    return (
-        <div className="about-me-page" id="about_me">
-            About Me Page
+      <div className="about-me-text">
+        <h1> About Me </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          illo, praesentium reiciendis quae asperiores ipsum excepturi sapiente
+          placeat tempore assumenda soluta provident, vitae minus autem sed?
+          Quos animi ad omnis voluptatem repellat eius accusamus cumque dicta
+          minus sunt minima dolorum, quae molestiae odit maiores soluta. Eius
+          aut iusto illo rem? Sapiente perspiciatis dolores consequuntur
+          expedita blanditiis, rerum iusto, optio nihil aspernatur neque facilis
+          quae aut tenetur! Corporis eligendi rem, eos doloribus velit
+          doloremque porro quasi corrupti minus! Exercitationem, provident
+          magni? Modi non aliquam magni, animi assumenda voluptatem rerum nisi
+          molestiae consectetur repellat ex laudantium veniam autem,
+          perspiciatis nulla? Ipsum, enim!{" "}
+        </p>
+
+        <h1>Technical Skills</h1>
+        <div className="about-me-languages">
+            <img src ={require("../assets/javascript-plain.svg")} height="50px" width="50px" alt="javascript" />
+            <span>Javascript</span>
         </div>
-    )
-    
-}
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
