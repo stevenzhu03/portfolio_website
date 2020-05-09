3631ed680dbe1d6dac8ec0eb49bf350b520fd401
Steven Zhu <stevenzhu03@gmail.com>
2020-05-08 23:57:16 -0400

    adding overlay to project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,11 +6,16 @@ const ProjectCard = props => {
         <div className="project-card">
             <img src ="https://c402277.ssl.cf1.rackcdn.com/photos/18366/images/carousel_small/Asian_Elephants_WW252891.jpg?1576701543" alt="something"/>
 
-            <p>Description</p>
+            <p>Name of Porject</p>
 
             <div className="project-card-icons">
                 <img alt="github"/>
                 <img alt="live-version"/>
+            </div>
+
+            <div className="project-card-overlay">
+                <p> Description </p>
+                <p> Techonologies Used</p>
             </div>
         </div>
     )
